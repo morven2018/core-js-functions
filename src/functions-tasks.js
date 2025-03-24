3ae710ab569084e2d162dcb88e43240d041b9bbe
morven2018 <you@example.com>
2025-03-24 21:44:18 +0300

    feat: add retry function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -138,8 +138,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let err;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        err = error;
+      }
+    }
+    throw err;
+  };
 }
 
 /**
