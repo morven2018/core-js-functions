92ae7dd6840c9667e7e648f9aa84e0be435f6a8b
morven2018 <you@example.com>
2025-03-24 22:32:14 +0300

    feat: add generator

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -219,8 +219,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let value = startFrom - 1;
+  return function () {
+    value += 1;
+    return value;
+  };
 }
 
 module.exports = {
