14971210686549871eb12e783cf8424c4fde688c
morven2018 <you@example.com>
2025-03-24 22:17:15 +0300

    feat add logger function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -175,8 +175,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const data = `${func.name}(${args.map((item) => JSON.stringify(item))})`;
+    logFunc(`${data} starts`);
+    const res = func(...args);
+    logFunc(`${data} ends`);
+    return res;
+  };
 }
 
 /**
